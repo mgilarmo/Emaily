d466bf497ddc89cf473003714b69f0dfc171b912
Matt Gilarmo <mgilarmo@gmail.com>
2019-06-05 23:12:57 -0400

    tweak google strategey settings

--- services/passport.js
+++ services/passport.js
@@ -21,7 +21,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClentSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true
     }, 
     (accessToken, refreshToken, profile, done) => {
       // anytime we reach out to database, we initiate an async action
